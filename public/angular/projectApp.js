2bbdf3e746180ffc12b9f474b31cd6799170a514
niravshah <nirav.shah83@gmail.com>
2016-07-10 21:00:02 +0100

    Updates

--- public/angular/projectApp.js
+++ public/angular/projectApp.js
@@ -1,30 +1,34 @@
-var app = angular.module('engageApp', ['ngStorage','ui.router']);
+var app = angular.module('engageApp', ['ngStorage', 'ui.router']);
 app.config(function ($interpolateProvider, $stateProvider, $urlRouterProvider) {
     $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
     $urlRouterProvider.otherwise('/home');
     $stateProvider
         .state('home', {
             url: '/home',
-            templateUrl: '/angular/partials/projectBody.html',
-            resolve:{
-                pid:function(){return $("#content").data("pid")}
+            views: {
+                'projectInfo': {
+                    templateUrl: '/angular/partials/projectInfo.html'
+                },
+                'teamMembers': {
+                    templateUrl: '/angular/partials/teamMembers.html'
+                },
+                'messageStream': {
+                    templateUrl: '/angular/partials/messageStream.html'
+                },
+                'projectTracker': {
+                    templateUrl: '/angular/partials/projectTracker.html'
+                }
             },
-            controller:'projectCardsCtrl'
+            resolve: {
+                pid: function () {
+                    return $("#content").data("pid")
+                }
+            }
 
         });
 });
 
-app.controller('projectCardsCtrl', ['$scope','pid', function ($scope,pid) {
-    $scope.testdata = [1,2,3];
-    $scope.pid = pid;
-}]);
 
-app.service('ProjectsService',['$http',function($http){
-    this.getProjectById = function(id){
-        return $http.get('/api/projects/'+id);
-    }
-}]);
-
-app.controller('headerController', function($scope,$localStorage) {
+app.controller('headerController', function ($scope, $localStorage) {
     $scope.token = $localStorage.currentUser.token;
 });
