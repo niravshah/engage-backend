f85317ef4953f9dd1a75ab9de2c76890badbf957
niravshah <nirav.shah83@gmail.com>
2016-08-25 23:58:47 +0100

    Messages

--- angular/controllers/admin/mainAdminController.js
+++ angular/controllers/admin/mainAdminController.js
@@ -1,30 +1,46 @@
-app.controller('mainAdminController', function ($window, $http, $scope,Upload, notify,usSpinnerService) {
+app.controller('mainAdminController', function ($window, $http, $scope, Upload, notify, usSpinnerService) {
 
-    $scope.init = function(){
+    $scope.init = function () {
         var $sidebar = angular.element('#contentw');
-        if(!$sidebar.hasClass('sidebar-show'))
-            $sidebar.removeClass('sidebar-hide');{
+        if (!$sidebar.hasClass('sidebar-show'))
+        {
+            $sidebar.removeClass('sidebar-hide');
             $sidebar.addClass('sidebar-show');
         }
+        $scope.szCategoryConfig = {
+            valueField: 'value',
+            labelField: 'label',
+            placeholder: 'Select Category'
+        };
+
+        $scope.szCategoryOptions = [
+            {label: 'Label 1', value: 'Value 1'},
+            {label: 'Label 2', value: 'Value 2'}
+        ];
+
+        $scope.currentProject = {};
+        $scope.currentProject.sDate = new Date();
+        $scope.currentProject.eDate = new Date();
+
     };
 
     $scope.init();
 
-    $scope.uploadUsers = function(){
+    $scope.uploadUsers = function () {
         var files = [];
-        if(typeof $scope.uploadedXls == 'object'){
+        if (typeof $scope.uploadedXls == 'object') {
             files.push($scope.uploadedXls);
         }
-        $scope.upload(files,{},'/api/upload/users',function(resp,err){
-           if(err){
-               notify('Error uploading users' + err.message);
-           } else{
-               if(resp.data.success == true) {
-                   notify('Users Upload Successful.' + resp.data.message);
-               }else{
-                   notify('Users Upload Failed.' + resp.data.message);
-               }
-           }
+        $scope.upload(files, {}, '/api/upload/users', function (resp, err) {
+            if (err) {
+                notify('Error uploading users' + err.message);
+            } else {
+                if (resp.data.success == true) {
+                    notify('Users Upload Successful.' + resp.data.message);
+                } else {
+                    notify('Users Upload Failed.' + resp.data.message);
+                }
+            }
         });
     };
 
@@ -39,6 +55,10 @@ app.controller('mainAdminController', function ($window, $http, $scope,Upload, n
         }, function (err) {
             cb(null, err);
         });
+    };
+
+    $scope.inputOnTimeSet = function (newDate, oldDate) {
+        $('#dLabel1').dropdown('toggle');
     };
 
 });
